d50f98bb649a52217539ecf805ba229b5f78bb74
Krishn Mehta <krishnmehta11@gmail.com>
2023-04-05 17:15:00 +0530

    Assignment-2

---
+++ src/StudentAssignment.EntityFrameworkCore/Migrations/20230405114419_Created_Student_Entity.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentAssignment.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedStudentEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AppStudents",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppStudents_Email",
+                table: "AppStudents",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppStudents_Email",
+                table: "AppStudents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AppStudents",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
